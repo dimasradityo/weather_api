aca6315f22e8399e36dee7e7bc3fae4fe40cd30b
Dimas Radityo <dimsrad307@gmail.com>
2024-12-07 02:21:29 +0700

    Return all temperature data for a station

--- main.py
+++ main.py
@@ -6,26 +6,35 @@ app = Flask('__name__')
 
 stations_reference = pd.read_csv('data_small/stations.txt', skiprows=17)
 
-def get_temperature(station_id, observation_date):
+def clean_data(station_id):
     station_filename = 'TG_STAID'+str(station_id).zfill(6)+'.txt'
     df = pd.read_csv('data_small/' + station_filename, skiprows=20, parse_dates=['    DATE'])
     df['TG0'] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
     df['TG'] = df['TG0']/10
-    return df.loc[df['    DATE']==observation_date]['TG'].squeeze()
+    return df
 
 
+# Homepage 
 @app.route('/')
 def home():
     return render_template('home.html', data=stations_reference.to_html())
 
+# GET temperature data for station and date
 @app.route('/api/v1/<station>/<date>')
 def about(station, date):
-    temperature = get_temperature(station, date)
+    df = clean_data(station)
+    temperature = df.loc[df['    DATE']==date]['TG'].squeeze()
     return {
 "station": station,
 "date": date,
 "temperature": temperature
     }
 
+# GET all temperature data for a station
+@app.route('/api/v1/<station>')
+def get_all_temperature_for_station(station):
+    df = clean_data(station)
+    return df[['    DATE','TG']].to_dict(orient='records')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
