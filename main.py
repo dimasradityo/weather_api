157067722ffe5e02e067f05f0445839f0a891fe8
Dimas Radityo <dimsrad307@gmail.com>
2024-12-07 01:48:03 +0700

    return temperature for any station and date

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 from flask import Flask, render_template
+import pandas as pd
+import numpy as np
 
 app = Flask('__name__')
+
+def get_temperature(station_id, observation_date):
+    station_filename = 'TG_STAID'+str(station_id).zfill(6)+'.txt'
+    df = pd.read_csv('data_small/' + station_filename, skiprows=20, parse_dates=['    DATE'])
+    df['TG0'] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
+    df['TG'] = df['TG0']/10
+    return df.loc[df['    DATE']==observation_date]['TG'].squeeze()
+
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# @app.route('/api/v1/<station>/<date>')
-# def about(station, date):
-#     return {
-# "station": station,
-# "date": date
-#     }
-
-@app.route('/api/v1/<keyword>')
-def api(keyword):
+@app.route('/api/v1/<station>/<date>')
+def about(station, date):
+    temperature = get_temperature(station, date)
     return {
-"definition": keyword.toupper(),
-"keyword": keyword
+"station": station,
+"date": date,
+"temperature": temperature
     }
+
+# @app.route('/api/v1/<keyword>')
+# def api(keyword):
+#     return {
+# "definition": keyword.toupper(),
+# "keyword": keyword
+#     }
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
